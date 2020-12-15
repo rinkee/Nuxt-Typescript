54015b725585dc057bcadb8c7e482e370eddda6e
김성일 <gimseong-il@ilMm.local>
2020-12-15 23:25:48 +0900

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,103 @@
+<template>
+  <div class="px-2">
+    <header class="p-2 border">
+      <nuxt-link to="/"
+        ><svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="45"
+          height="35.185"
+          viewBox="0 0 45 35.185"
+        >
+          <g
+            id="그룹_14"
+            data-name="그룹 14"
+            transform="translate(-34 -59.022)"
+          >
+            <text
+              id="Project_Studio"
+              data-name="Project
+Studio"
+              transform="translate(34 73)"
+              font-size="12"
+              font-family="GmarketSansTTFLight, Gmarket Sans TTF"
+              letter-spacing="0.07em"
+            >
+              <tspan x="0" y="0">Project</tspan>
+              <tspan x="0" y="16">Studio</tspan>
+            </text>
+            <g
+              id="그룹_1"
+              data-name="그룹 1"
+              transform="translate(-44.604 -58.771)"
+            >
+              <path
+                id="패스_3"
+                data-name="패스 3"
+                d="M0,0H35.312"
+                transform="translate(82.085 123.5)"
+                fill="none"
+                stroke="#fe725b"
+                stroke-linecap="round"
+                stroke-width="1"
+              />
+              <path
+                id="패스_5"
+                data-name="패스 5"
+                d="M5,0,0,5"
+                transform="translate(117.397 118.5)"
+                fill="none"
+                stroke="#fe725b"
+                stroke-linecap="round"
+                stroke-width="1"
+              />
+              <path
+                id="패스_2"
+                data-name="패스 2"
+                d="M0,27.5V0"
+                transform="translate(117.397 124.271)"
+                fill="none"
+                stroke="#fe725b"
+                stroke-linecap="round"
+                stroke-width="1"
+              />
+              <path
+                id="패스_6"
+                data-name="패스 6"
+                d="M5,0,0,5"
+                transform="translate(117.397 147.271)"
+                fill="none"
+                stroke="#fe725b"
+                stroke-linecap="round"
+                stroke-width="1"
+              />
+              <path
+                id="패스_4"
+                data-name="패스 4"
+                d="M0,0V27.5"
+                transform="translate(81.5 124.271)"
+                fill="none"
+                stroke="#fe725b"
+                stroke-linecap="round"
+                stroke-width="1"
+              />
+            </g>
+          </g>
+        </svg>
+      </nuxt-link>
+    </header>
+    <Nuxt />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+
+@Component({})
+export default class MyClass extends Vue {}
+</script>
+
+<style ${2|scoped,|} lang="scss">
+header {
+  height: 56px;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,194 @@
+<template>
+  <div class="text-center">
+    <div class="mt-32">
+      <h1 class="text-3xl leading-9">
+        ProjectStudio는<br />
+        공동 작업자를<br />
+        더욱 편하게 찾아줍니다.
+      </h1>
+      <p class="text-gray-700 mt-4">
+        믿을 수 있는 개발자, 디자이너, 투자자를 만나보세요
+      </p>
+      <img src="@/assets/img/partner.png" alt="" />
+    </div>
+    <div class="mt-32">
+      <h1 class="text-3xl leading-9">
+        전세계 사람들과<br />
+        글로벌한 프로젝트를<br />
+        시작해보세요.
+      </h1>
+      <p class="text-gray-700 mt-4">
+        전문가에게 피드백을 받고 프로젝트를 발전시키세요
+      </p>
+      <img src="@/assets/img/world.png" alt="" />
+    </div>
+    <svg
+      class="mx-auto"
+      xmlns="http://www.w3.org/2000/svg"
+      width="4"
+      height="68"
+      viewBox="0 0 4 68"
+    >
+      <line
+        id="선_3"
+        data-name="선 3"
+        y2="68"
+        transform="translate(2)"
+        fill="none"
+        stroke="#ee6c70"
+        stroke-width="4"
+      />
+    </svg>
+    <div class="mt-32">
+      <h1 class="text-3xl leading-9">
+        프로들의 <br />
+        Communication
+      </h1>
+      <p class="text-gray-700 mt-4">
+        전문가에게 피드백을 받고 프로젝트를 발전시키세요
+      </p>
+      <svg
+        class="mx-auto mt-10"
+        xmlns="http://www.w3.org/2000/svg"
+        width="335"
+        height="220"
+        viewBox="0 0 335 220"
+      >
+        <g id="그룹_17" data-name="그룹 17" transform="translate(0 0.135)">
+          <g id="그룹_12" data-name="그룹 12" transform="translate(0 -0.135)">
+            <g id="그룹_7" data-name="그룹 7">
+              <rect
+                id="사각형_7"
+                data-name="사각형 7"
+                width="154"
+                height="40"
+                rx="12"
+                transform="translate(7)"
+                fill="#eb5c60"
+              />
+              <path
+                id="다각형_1"
+                data-name="다각형 1"
+                d="M9,0l9,13H0Z"
+                transform="translate(0 27)"
+                fill="#eb5c60"
+              />
+            </g>
+            <text
+              id="이런_디자인은_어때요_"
+              data-name="이런 디자인은 어때요?"
+              transform="translate(86 25)"
+              fill="#fff"
+              font-size="14"
+              font-family="AppleSDGothicNeo-Regular, Apple SD Gothic Neo"
+            >
+              <tspan x="-61.334" y="0">이런 디자인은 어때요?</tspan>
+            </text>
+          </g>
+          <g id="그룹_13" data-name="그룹 13" transform="translate(0 119.775)">
+            <g id="그룹_8" data-name="그룹 8">
+              <rect
+                id="사각형_7-2"
+                data-name="사각형 7"
+                width="99"
+                height="40"
+                rx="12"
+                transform="translate(7)"
+                fill="#eb5c60"
+              />
+              <path
+                id="다각형_1-2"
+                data-name="다각형 1"
+                d="M9,0l9,15H0Z"
+                transform="translate(0 25)"
+                fill="#eb5c60"
+              />
+            </g>
+            <text
+              id="다행이네요_"
+              data-name="다행이네요 !"
+              transform="translate(58 25)"
+              fill="#fff"
+              font-size="14"
+              font-family="AppleSDGothicNeo-Regular, Apple SD Gothic Neo"
+            >
+              <tspan x="-33.922" y="0">다행이네요 !</tspan>
+            </text>
+          </g>
+          <g id="그룹_10" data-name="그룹 10" transform="translate(219 59.82)">
+            <g id="그룹_9" data-name="그룹 9">
+              <rect
+                id="사각형_7-3"
+                data-name="사각형 7"
+                width="109"
+                height="40"
+                rx="12"
+                fill="#953538"
+              />
+              <path
+                id="다각형_1-3"
+                data-name="다각형 1"
+                d="M8.5,0,17,13H0Z"
+                transform="translate(99 27)"
+                fill="#953538"
+              />
+            </g>
+            <text
+              id="마음에_들어요_"
+              data-name="마음에 들어요 !"
+              transform="translate(54 25)"
+              fill="#fff"
+              font-size="14"
+              font-family="AppleSDGothicNeo-Regular, Apple SD Gothic Neo"
+            >
+              <tspan x="-41.818" y="0">마음에 들어요 !</tspan>
+            </text>
+          </g>
+          <g id="그룹_11" data-name="그룹 11" transform="translate(201 179.73)">
+            <g id="그룹_9-2" data-name="그룹 9">
+              <rect
+                id="사각형_7-4"
+                data-name="사각형 7"
+                width="127"
+                height="40"
+                rx="12"
+                fill="#953538"
+              />
+              <path
+                id="패스_7"
+                data-name="패스 7"
+                d="M8.5,0,17,13H0Z"
+                transform="translate(117 27.135)"
+                fill="#953538"
+              />
+            </g>
+            <text
+              id="정말_감사합니다_"
+              data-name="정말 감사합니다 ;)"
+              transform="translate(66 25)"
+              fill="#fff"
+              font-size="14"
+              font-family="AppleSDGothicNeo-Regular, Apple SD Gothic Neo"
+            >
+              <tspan x="-50.274" y="0">정말 감사합니다 ;)</tspan>
+            </text>
+          </g>
+        </g>
+      </svg>
+      <v-btn color="purple">text</v-btn>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+
+@Component({})
+export default class MyClass extends Vue {}
+</script>
+
+<style ${2|scoped,|} lang="scss">
+.mt-100 {
+  margin-top: 100px;
+}
+</style>
